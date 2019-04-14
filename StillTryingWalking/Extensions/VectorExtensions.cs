669115565142a8c730b5464d63dd60b121585df9
adnanioricce <adnanioricce@outlook.com>
2019-04-14 14:45:43 -0300

    first part maded

---
+++ StillTryingWalking/Extensions/VectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StillTryingWalking.Extensions
+{
+    public static class VectorExtensions
+    {
+        //public static Vector2 Walk(this Vector2 vector)
+        //{
+            
+        //}
+    }
+}
---
+++ StillTryingWalking/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using StillTryingWalking.Extensions;
+using System.Collections.Generic;
+
+namespace StillTryingWalking
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Vector2 startPosition; 
+        List<Pixel> pixels;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }        
+        protected override void Initialize()
+        {
+            pixels = new List<Pixel>();
+            startPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            pixels.Add(new Pixel(GraphicsDevice, startPosition ));
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            var walk = new Vector2((float)(new Random().NextDouble()), (float)(new Random().NextDouble()));
+            var nextPosition = startPosition + walk;
+            startPosition = nextPosition;
+            pixels.Add(new Pixel(GraphicsDevice,nextPosition));
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.Deferred);
+            foreach (Pixel pixel in pixels)
+            {
+                spriteBatch.Draw(pixel.texture, pixel.position, Color.Black);
+            }            
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
